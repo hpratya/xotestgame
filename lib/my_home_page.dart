00a6d4a58d996609053162b8625e942c9f410dc1
Pratya Homhuan <pratya4xd@gmail.com>
2021-11-19 15:40:49 +0700

    add animation gif

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,10 +1,13 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_new
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:myxotestgame/custome_dialog.dart';
 import 'package:myxotestgame/game_button.dart';
 import 'dart:math';
+import 'package:screen_recorder/screen_recorder.dart';
+import 'package:custom_check_box/custom_check_box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,6 +24,9 @@ class _HomePageState extends State<HomePage> {
   int nt = 3, ntt = 1;
   double screen = 0;
   double hscreen = 0;
+  DateTime time_now = DateTime.now();
+  bool shouldCheck = false;
+  bool shouldCheckDefault = false;
 
   @override
   void initState() {
@@ -28,6 +34,9 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     buttonsList = doInit();
   }
+
+  ScreenRecorderController controller =
+      ScreenRecorderController(pixelRatio: 3, skipFramesBetweenCaptures: 1);
 
   List<GameButton> doInit() {
     int ntt = nt * nt, i = 1;
@@ -45,6 +54,7 @@ class _HomePageState extends State<HomePage> {
 
   void playGame(GameButton gb) {
     setState(() {
+      //startScreenRecording(time_now);
       if (activePlayer == 1) {
         gb.text = "X";
         gb.bg = Colors.red;
@@ -56,6 +66,7 @@ class _HomePageState extends State<HomePage> {
         activePlayer = 1;
         player2.add(gb.id);
       }
+      //stopScreenRecording();
       gb.enabled = false;
       int winner = checkWinner();
 
@@ -63,6 +74,7 @@ class _HomePageState extends State<HomePage> {
         if (buttonsList.every((p) => p.text != "")) {
           showDialog(
               context: context,
+              // ignore: unnecessary_new
               builder: (_) => new CustomDialog("Game Tied",
                   "Press the reset button to start again.", resetGame));
         } else {
@@ -87,6 +99,41 @@ class _HomePageState extends State<HomePage> {
     int i = buttonsList.indexWhere((p) => p.id == cellID);
     playGame(buttonsList[i]);
   }
+
+  // int checkWinnerrr() {
+  //   var winner = -1;
+  //   var countPoint = 0;
+  //   for (var i = 1; i <= nt * nt; i++) {
+  //     player1.contains(i);
+  //     if (countPoint == nt) {
+  //       //var sum = player1.reduce((a, b) => a + b);
+  //       winner = 1;
+  //     }
+  //   }
+  //   //row 1
+  //   // if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+  //   //   winner = 1;
+  //   // }
+  //   // if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+  //   //   winner = 2;
+  //   // }
+  //   if (winner != -1) {
+  //     if (winner == 1) {
+  //       showDialog(
+  //           context: context,
+  //           // ignore: unnecessary_new
+  //           builder: (_) => new CustomDialog("Player 1 Won",
+  //               "Press the reset button to start again.", resetGame));
+  //     } else {
+  //       showDialog(
+  //           context: context,
+  //           // ignore: unnecessary_new
+  //           builder: (_) => new CustomDialog("Player 2 Won",
+  //               "Press the reset button to start again.", resetGame));
+  //     }
+  //   }
+  //   return winner;
+  // }
 
   int checkWinner() {
     var winner = -1;
@@ -161,11 +208,13 @@ class _HomePageState extends State<HomePage> {
       if (winner == 1) {
         showDialog(
             context: context,
+            // ignore: unnecessary_new
             builder: (_) => new CustomDialog("Player 1 Won",
                 "Press the reset button to start again.", resetGame));
       } else {
         showDialog(
             context: context,
+            // ignore: unnecessary_new
             builder: (_) => new CustomDialog("Player 2 Won",
                 "Press the reset button to start again.", resetGame));
       }
@@ -202,9 +251,11 @@ class _HomePageState extends State<HomePage> {
       decoration: BoxDecoration(
           color: Colors.white54, borderRadius: BorderRadius.circular(25)),
       margin: EdgeInsets.only(top: 10),
-      width: screen * 0.75,
+      width: screen * 0.50,
+      height: 40,
       //height: screen * 0.15,
       child: TextField(
+        textAlign: TextAlign.center,
         keyboardType: TextInputType.number,
         inputFormatters: <TextInputFormatter>[
           FilteringTextInputFormatter.digitsOnly
@@ -215,15 +266,16 @@ class _HomePageState extends State<HomePage> {
             nt = 3;
             showDialog(
                 context: context,
+                // ignore: unnecessary_new
                 builder: (_) => new CustomDialog("Dimention must in Positive",
-                    "Press the reset button to start again.", resetGame));
+                    'Press the reset button to start again.', resetGame));
           } else {
             resetGame();
           }
         },
         style: TextStyle(color: Colors.black),
         decoration: InputDecoration(
-          hintText: 'Enter Number of Dimention XO',
+          hintText: 'Dimention',
           enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(25),
               borderSide: BorderSide(color: Colors.blue)),
@@ -243,43 +295,164 @@ class _HomePageState extends State<HomePage> {
         title: Text("XO"),
         backgroundColor: Colors.black,
       ),
+      resizeToAvoidBottomInset: false,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(10.0),
-              itemCount: buttonsList.length,
-              itemBuilder: (context, i) => SizedBox(
-                width: 100.0,
-                height: hscreen * 0.80,
-                child: Padding(
-                  padding: EdgeInsets.all(2.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(onPrimary: Colors.black),
-                    onPressed: buttonsList[i].enabled
-                        ? () => playGame(buttonsList[i])
-                        : null,
-                    child: Text(
-                      buttonsList[i].text,
-                      style: TextStyle(color: Colors.black, fontSize: 15.0),
-                      textAlign: TextAlign.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ScreenRecorder(
+              height: 425,
+              width: 200,
+              background: Colors.white,
+              controller: controller,
+              child: GridView.builder(
+                padding: const EdgeInsets.all(10.0),
+                itemCount: buttonsList.length,
+                itemBuilder: (context, i) => SizedBox(
+                  width: 100.0,
+                  height: hscreen * 0.80,
+                  child: Padding(
+                    padding: EdgeInsets.all(2.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(onPrimary: Colors.black),
+                      onPressed: buttonsList[i].enabled
+                          ? () => playGame(buttonsList[i])
+                          : null,
+                      child: Text(
+                        buttonsList[i].text,
+                        style: TextStyle(color: Colors.black, fontSize: 15.0),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: nt, //ค่ารับเข้าจำนวนเมตริก
+                    childAspectRatio: 1.0,
+                    crossAxisSpacing: 1.5,
+                    mainAxisSpacing: 1.5),
               ),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: nt, //ค่ารับเข้าจำนวนเมตริก
-                  childAspectRatio: 1.0,
-                  crossAxisSpacing: 1.5,
-                  mainAxisSpacing: 1.5),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  buildDimention(screen),
+                  buildReset(),
+                  buildStartStopRecorder()
+                ],
+              ),
+            ),
+            // buildDimention(screen),
+            // buildReset(),
+            // buildStartStopRecorder(),
+          ]),
+    );
+  }
+
+  List<Widget> get build_XO_GamePad {
+    return <Widget>[
+      Expanded(
+        child: GridView.builder(
+          padding: const EdgeInsets.all(10.0),
+          itemCount: buttonsList.length,
+          itemBuilder: (context, i) => SizedBox(
+            width: 100.0,
+            height: hscreen * 0.80,
+            child: Padding(
+              padding: EdgeInsets.all(2.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(onPrimary: Colors.black),
+                onPressed: buttonsList[i].enabled
+                    ? () => playGame(buttonsList[i])
+                    : null,
+                child: Text(
+                  buttonsList[i].text,
+                  style: TextStyle(color: Colors.black, fontSize: 15.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ),
-          buildDimention(screen),
-          buildReset()
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: nt, //ค่ารับเข้าจำนวนเมตริก
+              childAspectRatio: 1.0,
+              crossAxisSpacing: 1.5,
+              mainAxisSpacing: 1.5),
+        ),
+      ),
+      //buildCheckBox(),
+      buildDimention(screen),
+      buildReset(),
+      buildStartStopRecorder(),
+    ];
+  }
+
+  Container buildStartStopRecorder() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      height: 35.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              controller.start();
+            },
+            child: Text('Start'),
+          ),
+          Container(
+            child: ElevatedButton(
+              onPressed: () async {
+                timeDilation = 2.0;
+                dynamic gif = await controller.export();
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Image.memory(
+                        gif,
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('show recoded video'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              controller.stop();
+            },
+            child: Text('Stop'),
+          ),
         ],
       ),
+    );
+  }
+
+  Row buildCheckBox() {
+    return Row(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        CustomCheckBox(
+            value: shouldCheckDefault,
+            splashColor: Colors.red.withOpacity(0.4),
+            tooltip: 'Check save History',
+            borderColor: Colors.red,
+            checkedFillColor: Colors.green,
+            borderRadius: 8,
+            borderWidth: 1,
+            onChanged: (val) {
+              setState(() {
+                shouldCheckDefault = val;
+                shouldCheck = true;
+              });
+            }),
+        Text("Check Box"),
+      ],
     );
   }
 }
